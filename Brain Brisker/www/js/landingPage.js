1183397c14245937bc3286fc74a7b6e4805c487d
DESKTOP-A6M713R\VisFarz <shruthakeerthis1405@gmail.com>
2017-12-25 19:36:48 +0530

    landing page data

---
+++ Brain Brisker/www/js/landingPage.js
@@ -0,0 +1,41 @@
+
+        var val,val1;
+        function stored()
+            {  
+                //Get Username,email-id and age from localhost
+                localStorage.name=document.getElementById("username").value;
+                 val=localStorage.getItem("name");
+                console.log(val);
+                document.getElementById("result").innerHTML="welcome"+":"+localStorage.name;
+                localStorage.mail=document.getElementById("email").value;
+                val1=localStorage.getItem("mail");
+                console.log(val1);
+                /*localStorage.age1=document.getElementById("age").value;
+                val2 =localStorage.getItem("age1");
+                console.log(val2); */
+                alert("your data will be stored");
+                modal.style.display = "none";
+            }
+            
+        function loading()
+            {
+                document.getElementById("username").value=localStorage.getItem("name");
+                document.getElementById("result").innerHTML="welcome"+":"+localStorage.name;
+                document.getElementById("email").value=localStorage.getItem("mail");
+    
+        if(val="")
+                {
+                document.getElementById("myModal").style.display="block";
+                }
+        else
+                {
+                document.getElementById("myModal").style.display="none";
+                }
+            }
+
+        function resett()
+                {
+                    document.getElementById("username").value=""; 
+                    document.getElementById("result").innerHTML="";
+                    document.getElementById("email").value="";                      
+                }    
